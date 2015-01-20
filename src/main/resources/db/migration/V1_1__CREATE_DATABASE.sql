7095e9ecb68c16f1a1e623cf01e08897ae594050
Marcelo Rodrigues <marcelosrodrigues@globo.com>
2015-01-19 22:45:38 -0200

    Versão inicial do projeto

--- src/main/resources/db/migration/V1_1__CREATE_DATABASE.sql
+++ src/main/resources/db/migration/V1_1__CREATE_DATABASE.sql
@@ -7,9 +7,6 @@ SET client_min_messages = warning;
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
 SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
